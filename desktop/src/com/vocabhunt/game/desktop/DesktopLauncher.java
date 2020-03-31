aeec5594ae0d5e35c03cdad04759281863e1731d
Jirawat-rackz <58859651+Jirawat-rackz@users.noreply.github.com>
2020-04-01 05:21:19 +0700

    add vocabHuntGame

---
+++ desktop/src/com/vocabhunt/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.vocabhunt.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vocabhunt.game.VocabHuntGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new VocabHuntGame(), config);
+	}
+}
